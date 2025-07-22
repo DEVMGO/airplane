0270511a85f74322f5dbc06a38567f55581ab087
Mohammad Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2025-07-22 19:07:07 +0330

    edit style input

--- src/features/shared/components/input.tsx
+++ src/features/shared/components/input.tsx
@@ -10,15 +10,17 @@ const Input = ({ ...props }: Props) => {
     const [showPass, setShowPass] = useState(true)
     return (
         <div className="w-full flex items-start justify-start flex-col">
-            <div className={`w-full flex items-center justify-between border-b-2 ${props.error ? 'border-red-400' : 'border-gray-300'}`}>
+            <div className={`w-full flex items-center justify-between relative`}>
                 <input
                     {...props}
                     type={showPass ? props.type : 'text'}
-                    className={`w-full h-11 border-none outline-none focus:border-red-500 text-base text-gray-800 font-medium`}
+                    className={`w-full h-11 outline-none text-base text-gray-800 font-medium
+                    border-b-2 ${props.error ? 'border-red-400' : 'border-gray-300'} focus:!border-red-500`}
                 />
                 {props.type === 'password' &&
                     <button
                         type="button"
+                        className="absolute right-0"
                         onClick={() => setShowPass(!showPass)}
                     >
                         {showPass ? <EyeIcon fill="#6b7280" /> : <EyeSlashIcon fill="#6b7280" />}
